a9766156eecc3d3cb02837864c5cabe37ddbf89a
stephen_adah <mailstephenreal@gmail.com>
2024-06-21 16:23:20 +0100

    FEAT: get specific tenant details route implementation

--- backend/app/routes/tenant.py
+++ backend/app/routes/tenant.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from bson import ObjectId
+from bson.objectid import ObjectId
 from app import mongo
 from app.models.tenant import Tenant
 
@@ -22,8 +22,8 @@ def create_tenant():
         emergency_contact=data['emergencyContact'],
         lease_agreement_details=data['leaseAgreementDetails']
     )
-    mongo.db.tenants.insert_one(tenant.to_dict())
-    return jsonify({"msg": "Tenant created successfully"}), 201
+    insert_id = mongo.db.tenants.insert_one(tenant.to_dict()).insert_id
+    return jsonify({"msg": "Tenant created successfully", "tenantId": str(inserted_id)}), 201
 
 
 # Get all tenants
@@ -44,7 +44,24 @@ def get_all_tenants():
     return jsonify(tenant_list), 200
 
 
-# Get Specific Tenant Details
+# Get a Specific Tenant Details
+@tenant_bp.route('/api/admin/tenants/<tenant_id', methods=['GET'])
+def get_tenant(tenantId):
+    tenant = mongo.db.tenants.find_one({"_id": ObjectId(tenantId)})
+    if tenant:
+        return jsonify({
+            "name": tenant['name'],
+            "dob": tenant['dob'],
+            "sex": tenant['sex'],
+            "contact_details": tenant['contact_details'],
+            "emergency_contact": tenant['emergency_contact'],
+            "lease_agreement_details": tenant['lease_agreement_details']
+        }), 200
+    else:
+        return jsonify({"error": "Tenant not found"}), 404
+
+
+# Update Specific Tenant Details
 @tenant_bp.route('/api/admin/tenants/<tenant_id', methods=['PUT'])
 def update_tenant(tenant_id):
     """update a specific tenant with a tenant_id.
