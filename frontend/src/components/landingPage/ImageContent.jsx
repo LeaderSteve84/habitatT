e612db5b74de1afe93bfd6ee0fa9603465d137fc
Airme1 <emmanuelochoja@yahoo.com>
2024-06-21 17:37:34 +0100

    image card adjusted

--- frontend/src/components/landingPage/ImageContent.jsx
+++ frontend/src/components/landingPage/ImageContent.jsx
@@ -1,12 +1,12 @@
-export default function ImageContent({ images }) {
+export default function ImageContent({ images, ...props}) {
     return (
         <div className="flex">
             {images.map((image, index) => (
                 <div key={index} className="bg-stone-200 rounded overflow-hidden m-2">
-                    <img src={image.src} alt={image.description} className="h-8"/>
-                    <div className="p-2 bg-green-300 w-full">
-                        <h2 className="text-stone-600 text-lg">{image.title}</h2>
-                        <p className="text-sm text-red-300">{image.message}</p>
+                    <img src={image.src} alt={image.description} className="h-64 w-80 object-cover"/>
+                    <div className="p-2 bg-green-300">
+                        <h2 className="text-stone-600 font-bold text-lg">{image.title}</h2>
+                        <p className="text-sm font-bold text-red-800">{image.message}</p>
                     </div>
                 </div>
             ))}
