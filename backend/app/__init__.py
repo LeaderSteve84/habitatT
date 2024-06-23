7553a15f529384aedd33613a579e539016e06974
stephen_adah <mailstephenreal@gmail.com>
2024-06-23 01:46:51 +0100

    PERF: restruction configuration and error handling

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python3
 """app package init"""
+
 from flask import Flask
-from flask_pymongo import PyMongo
-from dotenv import load_dotenv
-import os
 from flask_cors import CORS
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
+from app.config import Config
 
-# Here we load environment variables from .env file
-load_dotenv()
 
-mongo = PyMongo()
+# Function to initialize MongoDB client
+def init_mongo_client(connection_string: str) -> MongoClient:
+    try:
+        client = MongoClient(connection_string)
+        print("MongoDB client initialized successfully.")
+        return client
+    except errors.ConnectionError as e:
+        print(f"Error connecting to MongoDB: {e}")
+        raise
 
+# Mongo client initialization
+CONNECTION_STRING = Config.MONGO_URI
+mongo_client: MongoClient = init_mongo_client(CONNECTION_STRING)
+
+# Select database and collection from Atlas
+database: Database = mongo_client.get_database("habitatTdb")
+tenantsCollection: Collection = database.get_collection("tenants")
 
 def create_app():
     """return flask application"""
     app = Flask(__name__)
-    app.config.from_object('app.config.Config')
 
-    # Here we initialize MongoDB connection
-    mongo_uri = os.getenv('MONGO_URI')
-    mongo.init_app(app, uri=mongo_uri)
-
-    # We enable CORS for all domains on all routes
+    # Load configuration from app.config
+    app.config.from_object(Config)
+    
+    # Enable CORS for all domains on all routes
     CORS(app)
 
     with app.app_context():
         # Import routes here to avoid circular imports
-        # Import and register blueprints
-        from app.routes.hello import hello_bp
-        app.register_blueprint(hello_bp)
         from app.routes import bp
+
+        # register blueprints
         app.register_blueprint(bp)
 
     return app
