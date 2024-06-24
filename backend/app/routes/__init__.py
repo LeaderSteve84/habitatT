71a1f95e46a7cd7416d0363b43fa05d683ad400f
stephen_adah <mailstephenreal@gmail.com>
2024-06-24 06:31:13 +0100

    FEAT: register admin_message

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """routes package init"""
 from flask import Blueprint
-from app.routes import tenant
+from app.routes import tenant, admin_message
 
 bp = Blueprint('main', __name__)
 
 # Register the sub_blueprints to the main blueprint
 bp.register_blueprint(tenant.tenant_bp)
+bp.register_blueprint(admin_message.admin_message_bp)
