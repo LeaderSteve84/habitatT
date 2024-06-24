6e3109c95003e48afbe1d8bc4d46f7a7af9c0602
stephen_adah <mailstephenreal@gmail.com>
2024-06-24 15:30:05 +0100

    FEAT: register the property_bp into main bp

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """routes package init"""
 from flask import Blueprint
-from app.routes import tenant, admin_message
+from app.routes import tenant, admin_message, properties
 
 bp = Blueprint('main', __name__)
 
 # Register the sub_blueprints to the main blueprint
 bp.register_blueprint(tenant.tenant_bp)
 bp.register_blueprint(admin_message.admin_message_bp)
+bp.register_blueprint(properties.property_bp)
