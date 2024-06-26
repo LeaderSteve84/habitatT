654b69ce6a9abad3fd42adcd9778390d493e3a02
stephen_adah <mailstephenreal@gmail.com>
2024-06-26 17:42:06 +0100

    FEAT: register blueprint of log request into main bp

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """routes package init"""
 from flask import Blueprint
-from app.routes import tenant, admin_message, property, listing
+from app.routes import tenant, admin_message, property, listing, log_request
 
 bp = Blueprint('main', __name__)
 
@@ -10,3 +10,4 @@ bp.register_blueprint(tenant.tenant_bp)
 bp.register_blueprint(admin_message.admin_message_bp)
 bp.register_blueprint(property.property_bp)
 bp.register_blueprint(listing.listing_bp)
+bp.register_blueprint(log_request.log_request_bp)
