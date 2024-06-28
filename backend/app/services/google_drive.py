7cc1048ac7325c880f7d7170312a56473373bc38
stephen_adah <mailstephenreal@gmail.com>
2024-06-28 14:03:55 +0100

    STYLE: pycodestyle compliance

--- backend/app/services/google_drive.py
+++ backend/app/services/google_drive.py
@@ -17,8 +17,11 @@ credentials = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 service = build('drive', 'v3', credentials=credentials)
 
+
 def upload_file_to_drive(file_path, file_name, mime_type):
     file_metadata = {'name': file_name}
     media = MediaFileUpload(file_path, mimetype=mime_type)
-    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    file = service.files().create(
+        body=file_metadata, media_body=media, fields='id'
+    ).execute()
     return file.get('id')
