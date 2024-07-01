1f819e32bf799df0aeb06845a24b1a38c2558eba
Airme1 <emmanuelochoja@yahoo.com>
2024-07-01 13:42:46 +0100

    FEAT: page routing order

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './ErrorPage.jsx'
+
+import ChatBoard from './routes/ChatBoard.jsx'
+import App from './routes/App.jsx'
+import LogRequest from './routes/LogRequest.jsx'
+import Profile from './routes/Profile.jsx'
+import UploadDoc from './routes/UploadDoc.jsx'
+import ViewRequest from './routes/ViewRequest.jsx'
+import Home from './routes/Home.jsx'
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+    children: [
+      {
+        path: "/home/messages",
+        element: <ChatBoard />
+      },
+      {
+        path: "/home/request",
+        element: <LogRequest />,
+        children: [
+          {
+            path: "/home/request/view",
+            element: <ViewRequest />
+          }
+        ]
+      },
+      {
+        path: "/home/profile",
+        element: <Profile />
+      },
+      {
+        path: "/home/uploaddocument",
+        element: <UploadDoc />
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
