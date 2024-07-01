5953518e50bc05bf009b0ee553d605d2db8a8cc5
Airme1 <emmanuelochoja@yahoo.com>
2024-07-01 13:53:26 +0100

    FEAT: adding linking functionality to sidebar

--- frontend/src/components/sidebar/MainSideBar.jsx
+++ frontend/src/components/sidebar/MainSideBar.jsx
@@ -2,16 +2,27 @@ import Sidebar, { SidebarItem } from "./Sidebar"
 import { BiHome, BiMessage, BiLogOut } from "react-icons/bi"
 import { CgProfile } from "react-icons/cg"
 import { FaRegAddressBook } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
-export default function MainSideBar({name, email}) {
+export default function MainSideBar({ name, email }) {
     return (
         <div>
             <Sidebar name={name} email={email}>
-                <SidebarItem icon={<BiHome />} text="Home" />
-                <SidebarItem icon={<BiMessage />} text="Communication Board" />
-                <SidebarItem icon={<FaRegAddressBook />} text="Log Request" />
-                <SidebarItem icon={<CgProfile />} text="Profile" />
-                <SidebarItem icon={<BiLogOut />} text="Log Out" />
+                <Link to={`/home`}>
+                    <SidebarItem icon={<BiHome />} text="Home" />
+                </Link>
+                <Link to={`/home/messages`}>
+                    <SidebarItem icon={<BiMessage />} text="Communication Board" />
+                </Link>
+                <Link to={`/home/request`}>
+                    <SidebarItem icon={<FaRegAddressBook />} text="Log Request" />
+                </Link>
+                <Link to={`/home/profile`}>
+                    <SidebarItem icon={<CgProfile />} text="Profile" />
+                </Link>
+                <Link to={`/home/`}>
+                    <SidebarItem icon={<BiLogOut />} text="Log Out" />
+                </Link>
             </Sidebar>
         </div>
     )
