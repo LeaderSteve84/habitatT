ee2d68a26d2d33b95ae527cac9fdcaf1485dc3b4
Airme1 <emmanuelochoja@yahoo.com>
2024-07-03 14:40:11 +0100

    completed ImageContent component

--- frontend/src/components/landingPage/ImageContent.jsx
+++ frontend/src/components/landingPage/ImageContent.jsx
@@ -1,9 +1,9 @@
-export default function ImageContent({ images, ...props}) {
+export default function ImageContent({ images, className, ...props}) {
     return (
-        <div className="flex">
+        <div className={`${className}`}>
             {images.map((image, index) => (
                 <div key={index} className="bg-stone-200 rounded overflow-hidden m-2">
-                    <img src={image.src} alt={image.description} className="h-64 w-80 object-cover"/>
+                    <img src={image.src} alt={image.description} className={`${props.height || 'h-64'} ${props.width || 'w-80'} object-cover`}/>
                     <div className="p-2 bg-green-300">
                         <h2 className="text-stone-600 font-bold text-lg">{image.title}</h2>
                         <p className="text-sm font-bold text-red-800">{image.message}</p>
