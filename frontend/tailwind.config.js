11b338966fc9abc1a76822a23bf1e874cf3bce99
Airme1 <emmanuelochoja@yahoo.com>
2024-07-03 14:32:53 +0100

    config modifications

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary10: "#CCEFFB",
+        primary20: "#AAE5F8",
+        primary30: "#80D7F4",
+        primary40: "#55CAF0",
+        primary50: "#2BBDED",
+        primary60: "#00B0E9",
+        primary70: "#0093C2",
+        primary80: "#00759B",
+        primary90: "#005875",
+        primary100: "#003B4E",
+        primary110: "#00232F",
+        acent10: "#DBECE4",
+        acent20: "#C2DFD2",
+        acent30: "#A4CFBB",
+        acent40: "#86C0A5",
+        acent50: "#67B08E",
+        acent60: "#49A078",
+        acent70: "#3D8564",
+        acent80: "#316B50",
+        acent90: "#25503C",
+        acent100: "#183528",
+        acent110: "#0F2018",
+      },
+    },
   },
   plugins: [],
 };
