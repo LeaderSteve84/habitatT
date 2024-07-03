d68dc2d7b6de23db5f34ab4aede8e0986e7868a1
Airme1 <emmanuelochoja@yahoo.com>
2024-07-03 14:41:36 +0100

    Feat: completed navbar structure

--- frontend/src/components/landingPage/Navbar.jsx
+++ frontend/src/components/landingPage/Navbar.jsx
@@ -1,9 +1,7 @@
-import logo from '../../assets/logo.jpeg';
-
 export default function Navbar() {
     return (
         <div className="flex bg-stone-500 text-stone-50 justify-between items-center p-4">
-            <img src={logo} alt="logo image" className='h-16 w-16'/>
+            <img src="https://ik.imagekit.io/rmhnagyqw/habitatT/logo.png?updatedAt=1720008714144" alt="logo image" className='h-16 w-16' />
             <ul className="flex gap-20">
                 <li>Home</li>
                 <li>About Us</li>
