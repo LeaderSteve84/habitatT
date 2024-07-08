4207d906c26490f227b504642112efc682be0cdd
Airme1 <emmanuelochoja@yahoo.com>
2024-07-08 12:39:33 +0100

    add protected route

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from 'react-router-dom';
+import useAuth from './hooks/useAuth';
+
+const ProtectedRoute = ({ children }) => {
+    const { auth } = useAuth();
+
+    if (!auth?.accessToken) {
+        // Not authenticated, redirect to login page
+        return <Navigate to="/login" replace />;
+    }
+
+    return children;
+}
+
+export default ProtectedRoute;
