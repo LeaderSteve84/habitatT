81011e4c371401dd058de82e27fa7eb1c186cf55
stephen_adah <mailstephenreal@gmail.com>
2024-07-10 19:07:56 +0100

    FEAT: update

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,11 @@
 """module for flask application"""
 from app import create_app
 from flask_mail import Mail
+
 app = create_app()
-app.config.from_object('config.Config')
+app.config.from_object('app.config.Config')
 mail = Mail()
+
 
 if __name__ == "__main__":
     app.run()
