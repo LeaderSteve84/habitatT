7e089bc8d7d1dc5d1dff8bd361548b5cda7473e1
Airme1 <emmanuelochoja@yahoo.com>
2024-07-10 04:57:43 +0100

    adjust sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,50 +1,23 @@
-import { LuChevronFirst, LuChevronLast } from 'react-icons/lu';
-import { CgMoreVertical } from 'react-icons/cg';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext } from 'react';
 
 const SidebarContext = createContext();
 
 export default function Sidebar({ name, email, children }) {
-    const [expanded, setExpanded] = useState(true);
-
-    const toggleSidebar = () => {
-        setExpanded(prevExpanded => !prevExpanded);
-    };
-
     return (
-        <aside className={`bg-white text-stone-100 h-full fixed transition-all duration-300 ${expanded ? "w-64" : "w-16"}`}>
-            <nav className='h-full flex flex-col h-full bg-acent110 border-r shadow-sm'>
+        <aside className="bg-white text-stone-100 h-full fixed w-64">
+            <nav className='h-full flex flex-col bg-acent110 border-r shadow-sm'>
                 <div className='p-4 pb-2 flex justify-between items-center'>
-                    <img src="https://ik.imagekit.io/rmhnagyqw/habitatT/logo.png?updatedAt=1720008714144" className={`overflow-hidden transition-all ${expanded ? "w-24 ml-3" : "w-0"}`} alt="habitatT" />
-                    <button onClick={toggleSidebar} className='p-1.5 rounded-lg bg-gray-800 hover:bg-gray-500'>
-                        {expanded ? <LuChevronFirst /> : <LuChevronLast />}
-                    </button>
+                    <img src="https://ik.imagekit.io/rmhnagyqw/habitatT/logo.png?updatedAt=1720008714144" className="w-24 ml-3" alt="habitatT" />
                 </div>
-                <SidebarContext.Provider value={{ expanded }}>
+                <SidebarContext.Provider>
                     <ul className='flex-1 px-3'>{children}</ul>
                 </SidebarContext.Provider>
-
-                <div className='border-t flex p-3'>
-                    <img src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
-                        alt=""
-                        className='w-10 h-10 rounded-md'
-                    />
-                    <div className={`flex justify-between items-center transition-all overflow-hidden ${expanded ? "ml-3 w-full" : "w-0"}`}>
-                        <div className='leading-4'>
-                            <h4 className='font-semibold text-gray-900'>{name}</h4>
-                            <span className='text-xs text-gray-900'>{email}</span>
-                        </div>
-                        <CgMoreVertical size={20} />
-                    </div>
-                </div>
             </nav>
         </aside>
     );
 }
 
 export function SidebarItem({ icon, text, active, alert }) {
-    const { expanded } = useContext(SidebarContext);
-
     return (
         <li
             className={`
@@ -57,29 +30,12 @@ export function SidebarItem({ icon, text, active, alert }) {
                 }
             `}>
             {icon}
-            <span
-                className={`overflow-hidden transition-all whitespace-nowrap
-                    ${expanded ? "ml-3 w-full" : "w-0"}
-                `}
-            >
+            <span className="ml-3 w-full">
                 {text}
             </span>
             {alert && <div
-                className={`absolute right-2 w-2 h-2 rounded bg-indigo-400
-                    ${expanded ? "" : "top-2"}`
-                }>
-            </div>}
-
-            {!expanded && (
-                <div className={`
-                    absolute left-full rounded-md px-2 py-1 ml-6
-                    bg-indigo-100 text-sm
-                    invisible opacity-20 -translate-x-3 transition-all
-                    group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
-                `}>
-                    {text}
-                </div>
-            )}
+                className="absolute right-2 w-2 h-2 rounded bg-indigo-400"
+            ></div>}
         </li>
     );
 }
