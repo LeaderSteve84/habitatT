8a79af28677664a228714a893d199cd2ba074eb7
Airme1 <emmanuelochoja@yahoo.com>
2024-07-11 13:23:27 +0100

    Navbar link and image position

--- frontend/src/components/landingPage/Navbar.jsx
+++ frontend/src/components/landingPage/Navbar.jsx
@@ -5,14 +5,19 @@ export default function Navbar() {
         <div className="flex bg-stone-700 text-stone-50 justify-between items-center p-4 shadow-lg">
             <img src="https://ik.imagekit.io/rmhnagyqw/habitatT/logo.png?updatedAt=1720008714144" alt="logo" className='h-12 w-12 object-cover' />
             <ul className="flex gap-10">
-                <li className="relative group cursor-pointer">
-                    Home
-                    <span className="absolute left-0 bottom-0 w-0 h-1 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
-                </li>
-                <li className="relative group cursor-pointer">
-                    About Us
-                    <span className="absolute left-0 bottom-0 w-0 h-1 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
-                </li>
+                <Link to="/">
+                    <li className="relative group cursor-pointer">
+                        Home
+                        <span className="absolute left-0 bottom-0 w-0 h-1 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+                    </li>
+                </Link>
+                <Link to='/about'>
+
+                    <li className="relative group cursor-pointer">
+                        About Us
+                        <span className="absolute left-0 bottom-0 w-0 h-1 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+                    </li>
+                </Link>
                 <li className="relative group cursor-pointer">
                     Contact Us
                     <span className="absolute left-0 bottom-0 w-0 h-1 bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
