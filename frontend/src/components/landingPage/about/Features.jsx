6ebda55fc13553f6a64cbb9f6ff2223065a47f4d
Airme1 <emmanuelochoja@yahoo.com>
2024-07-11 12:13:48 +0100

    Feature: About Us page

---
+++ frontend/src/components/landingPage/about/Features.jsx
@@ -0,0 +1,18 @@
+const Features = () => {
+    return (
+        <section className="mb-6">
+            <h2 className="text-3xl font-semibold mb-4">Features</h2>
+            <ul className="list-disc ml-6">
+                <li>Secure User Login: Tenants and admins can log in securely.</li>
+                <li>Central Communication Dashboard: Access all messages and tasks from a single dashboard.</li>
+                <li>Task Management and Assignment: Stay organized with task assignments.</li>
+                <li>Maintenance Request Submission and Tracking: Streamline maintenance processes.</li>
+                <li>File Sharing and Document Storage: Collaborate seamlessly.</li>
+                <li>Real-Time Notifications and Alerts: Stay informed.</li>
+                <li>Reporting and Analytics: Gain valuable insights.</li>
+            </ul>
+        </section>
+    );
+};
+
+export default Features;
